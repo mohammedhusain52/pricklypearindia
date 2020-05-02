46cfb34422f184bb55a6577684c2c3f2bee5b3cd
mohammedhusain52 <51230733+mohammedhusain52@users.noreply.github.com>
2020-05-02 14:29:40 +0530

    Add files via upload

---
+++ wp-content/template-recipes.php
@@ -0,0 +1,13 @@
+<?php
+/*Template Name: Recipes*/
+get_header();
+query_posts(array(
+   'post_type' => 'recipes'
+)); ?>
+<?php
+while (have_posts()) : the_post(); ?>
+<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+<p><?php the_excerpt(); ?></p>
+<?php endwhile;
+get_footer();
+?>
